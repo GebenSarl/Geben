8b83c7bc50706c9efed91a4bb635f4d9e481b2ef
GebenSarl <gggebensarl@gmail.com>
2026-09-14 16:14:12 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Les données de gestion ne passent jamais par ici : elles restent dans le
    stockage local du téléphone. */
 
-const CACHE = "geben-v6";
+const CACHE = "geben-v8";
 
 const FICHIERS = [
   "./",
